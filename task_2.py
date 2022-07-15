503959e0ae56a3390ac37d4f6a7e5535b94b7cb1
Mansur <mansur.ham@mail.ru>
2022-07-15 12:40:11 +0300

    added 2 task

--- task_2.py
+++ task_2.py
@@ -1,16 +1,26 @@
+import pandas as pd
 import requests
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
 
-def wiki_parsing():
-    animals = []
-    url = "https://inlnk.ru/jElywR"
-    res = requests.get(url)
-    if res.status_code == 200:
-        soup = BS(res.content, 'html.parser')
-        ul = soup.find('div', class_="mw-category mw-category-columns")
-        li = ul.find_all('a', class_="")
-        for i in li:
-            title = i.text
-            print(title)
+def task():
+    URL="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83"
+    data=[]
+    for count in range(202):
+        html=requests.get(URL).text
+        soup = BeautifulSoup(html, 'html.parser')
+        for i in soup.find("div", class_="mw-category mw-category-columns"):
+            for j in i.find_all('li'):
+                data.append(j.get_text())
+        for i in soup.find_all("a",title="Категория:Животные по алфавиту"):
+            URL="https://ru.wikipedia.org"+i.get('href')
 
-wiki_parsing()
+    word="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    for i in word:
+        count=0
+        for j in data:
+            if j[0]==i:
+                count+=1
+        print(i,":",count)
+    print(len(data))
+    
+task()
