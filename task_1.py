b10551c4104fd0626301ca6d529a3cfc2d54699a
Mansur <mansur.ham@mail.ru>
2022-07-15 12:44:42 +0300

    added comments for 1 task

--- task_1.py
+++ task_1.py
@@ -1,9 +1,13 @@
 import array as arr 
 
+#вводим двоичный код (Целое число)
 massiv = int(float(input('Введите двоичный код')))
 
+#разбиваем целое число на цифры
 x = [int(a) for a in str(massiv)]
 
+#создаем массив из цифр
 numbers = arr.array('i',x)
 
+#выводим индекс первого нуля
 print(numbers.index(0))
