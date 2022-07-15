83bf3cf096ac1aa2b842477aebe70b5ad9870791
Mansur <mansur.ham@mail.ru>
2022-07-15 12:48:34 +0300

    added comments for 2 task

--- task_2.py
+++ task_2.py
@@ -2,9 +2,10 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+#создаем функцию
 def task():
     URL="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83"
-    data=[]
+    data=[] #список всех животных
     for count in range(202):
         html=requests.get(URL).text
         soup = BeautifulSoup(html, 'html.parser')
@@ -14,7 +15,8 @@ def task():
         for i in soup.find_all("a",title="Категория:Животные по алфавиту"):
             URL="https://ru.wikipedia.org"+i.get('href')
 
-    word="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    word="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" #алфавит для счетчика
+    # создаем цикл для подсчета имен по алфавиту
     for i in word:
         count=0
         for j in data:
