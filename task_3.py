72fe3cfe3a03a734a94f5b3c9dcb5d1b2e6ce45a
Mansur <mansur.ham@mail.ru>
2022-07-16 12:37:46 +0300

    added flake8

--- task_3.py
+++ task_3.py
@@ -14,7 +14,7 @@ def appearance(intervals):
         if timestump in lesson_range:
             pupil_result = check_in_range(timestump, pupil_ranges)
             tutor_result = check_in_range(timestump, tutor_ranges)
-            if pupil_result == True and tutor_result == True:
+            if pupil_result is True and tutor_result is True:
                 intervals_list.append(timestump)
     intervals_list.sort()
     time = 0
@@ -36,20 +36,23 @@ def make_ranges(intervals):
         range_list.append(range(intervals[i-1], intervals[i]+1))
     return range_list
 
+
 tests = [
     {'data': {'lesson': [1594663200, 1594666800],
-             'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
-             'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
+              'pupil': [1594663340, 1594663389, 1594663390,
+                        1594663395, 1594663396, 1594666472],
+              'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
      'answer': 3117
-    },
+     },
     {'data': {'lesson': [1594692000, 1594695600],
-             'pupil': [1594692033, 1594696347],
-             'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
-    'answer': 3565
-    },
+              'pupil': [1594692033, 1594696347],
+              'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
+     'answer': 3565
+     },
 ]
 
 for i, test in enumerate(tests):
     test_answer = appearance(test['data'])
     print(test_answer)
-    assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    assert test_answer == test['answer'], \
+        f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
